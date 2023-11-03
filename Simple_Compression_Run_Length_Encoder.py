4d4ae25eb5f28673d5a70f30671d4148f1446b86
Kooreis <kooreis.official@gmail.com>
2023-11-03 00:14:20 -0500

    Initializes the run_length_encoding function, sets up the encoding string and the index variable i.

--- Simple_Compression_Run_Length_Encoder.py
+++ Simple_Compression_Run_Length_Encoder.py
@@ -1,16 +1,3 @@
 def run_length_encoding(input_string):
     encoding = ''
     i = 0
-
-    while i < len(input_string):
-        count = 1
-        while i + 1 < len(input_string) and input_string[i] == input_string[i+1]:
-            i += 1
-            count += 1
-        encoding += str(count) + input_string[i]
-        i += 1
-    return encoding
-
-if __name__ == "__main__":
-    input_string = input("Enter the string to be encoded: ")
-    print(run_length_encoding(input_string))

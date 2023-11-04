24c30eff9d46750ac4c5cf85e9c0fdae962d6431
Kooreis <kooreis.official@gmail.com>
2023-11-04 00:14:20 -0500

    Created the main program class and the main method. The main method asks the user for a string to compress, reads the input, and then prints the compressed string.

--- Simple_Compression_Run_Length_Encoder.cs
+++ Simple_Compression_Run_Length_Encoder.cs
@@ -1,2 +1,8 @@
-using System;
-using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string to compress:");
+        string input = Console.ReadLine();
+        Console.WriteLine("Compressed string: " + RunLengthEncode(input));
+    }
